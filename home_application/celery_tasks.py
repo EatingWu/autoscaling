f1c0dd845f6640171461ed8485b4c92127d103ef
EatingWu <10ytwu@gmail.com>
2018-12-23 15:33:11 +0800

    增加主机信息功能3

--- home_application/celery_tasks.py
+++ home_application/celery_tasks.py
@@ -21,31 +21,45 @@ from celery.task import periodic_task
 
 from common.log import logger
 
+from home_application.models import HostInfo,CeleryHostInfo
+from home_application.vmware import get_host_info
 
 @task()
-def async_task(x, y):
-    """
-    定义一个 celery 异步任务
-    """
-    logger.error(u"celery 定时任务执行成功，执行结果：{:0>2}:{:0>2}".format(x, y))
-    return x + y
+def check_host_status():
+    hosts_dict_flag = False
+    hosts_dict_datalist = []
+    host_info_len = HostInfo.objects.count()
+    for host_info_id in range(1, host_info_len + 1):
+        #print host_info_id
+        ip_value = HostInfo.objects.filter(id=host_info_id).values('host_ip')[0].get('host_ip')
+        name_value = HostInfo.objects.filter(id=host_info_id).values('host_name')[0].get('host_name')
+        password_value = HostInfo.objects.filter(id=host_info_id).values('host_password')[0].get('host_password')
+        #print ip_value, name_value, password_value
+        hosts_dict_datalist = get_host_info(ip_value, name_value, password_value)
+        #print dir(CeleryHostInfo.objects)
+        hosts_dict_data_len = len(hosts_dict_datalist)
+        for host_info in range(0,hosts_dict_data_len):
+            #print(hosts_dict_datalist[host_info].get("hostname"))
+            host_name = CeleryHostInfo.objects.filter(host_name=hosts_dict_datalist[host_info].get("hostname")).values("host_name")
+            if host_name:
+                #print "exist"
+                CeleryHostInfo.objects.filter(host_name=hosts_dict_datalist[host_info].get('hostname')).update(host_ip=ip_value,
+                                              cpu_usage=hosts_dict_datalist[host_info].get('hostcpuusagepercent'),
+                                              mem_usage=hosts_dict_datalist[host_info].get('hostmemoryusagepercent'),
+                                              running_vms=hosts_dict_datalist[host_info].get('hostrunningvms'),
+                                              stopped_vms=hosts_dict_datalist[host_info].get('hoststoppedvms'),
+                                              total_vms=hosts_dict_datalist[host_info].get('hosttotalvms'),
+                                              status=hosts_dict_datalist[host_info].get('hoststatus'))
+            else:
+                #print "new"
+                CeleryHostInfo.objects.create(host_ip=ip_value, host_name=hosts_dict_datalist[host_info].get('hostname'),
+                                              cpu_usage=hosts_dict_datalist[host_info].get('hostcpuusagepercent'),
+                                              mem_usage=hosts_dict_datalist[host_info].get('hostmemoryusagepercent'),
+                                              running_vms=hosts_dict_datalist[host_info].get('hostrunningvms'),
+                                              stopped_vms=hosts_dict_datalist[host_info].get('hoststoppedvms'),
+                                              total_vms=hosts_dict_datalist[host_info].get('hosttotalvms'),
+                                              status=hosts_dict_datalist[host_info].get('hoststatus'))
 
-
-def execute_task():
-    """
-    执行 celery 异步任务
-
-    调用celery任务方法:
-        task.delay(arg1, arg2, kwarg1='x', kwarg2='y')
-        task.apply_async(args=[arg1, arg2], kwargs={'kwarg1': 'x', 'kwarg2': 'y'})
-        delay(): 简便方法，类似调用普通函数
-        apply_async(): 设置celery的额外执行选项时必须使用该方法，如定时（eta）等
-                      详见 ：http://celery.readthedocs.org/en/latest/userguide/calling.html
-    """
-    now = datetime.datetime.now()
-    logger.error(u"celery 定时任务启动，将在60s后执行，当前时间：{}".format(now))
-    # 调用定时任务
-    async_task.apply_async(args=[now.hour, now.minute], eta=now + datetime.timedelta(seconds=60))
 
 
 @periodic_task(run_every=crontab(minute='*/5', hour='*', day_of_week="*"))
@@ -56,6 +70,6 @@ def get_time():
     run_every=crontab(minute='*/5', hour='*', day_of_week="*")：每 5 分钟执行一次任务
     periodic_task：程序运行时自动触发周期任务
     """
-    execute_task()
+
     now = datetime.datetime.now()
     logger.error(u"celery 周期任务调用成功，当前时间：{}".format(now))
