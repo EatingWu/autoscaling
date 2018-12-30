c273b124b1b4fdb138cc7dd2ae8ceae1f2c30a34
EatingWu <10ytwu@gmail.com>
2018-12-30 23:37:33 +0800

    增加存储信息功能24

--- home_application/views.py
+++ home_application/views.py
@@ -134,15 +134,19 @@ def reset_vms_data(request):
     reset_vmnum = request.POST.get('resetdata_vmnum', '')
     reset_data = int(request.POST.get('resetdata', ''))
     host_name_value = HostInfo.objects.filter(host_ip=reset_hostip).values('host_name')[0].values()[0]
+    #DebugInfo.objects.create(text_info=host_name_value)
     host_password_value = HostInfo.objects.filter(host_ip=reset_hostip).values('host_password')[0].values()[0]
+    #DebugInfo.objects.create(text_info=host_password_value)
     vm_name_value = CeleryVMsLatestInfo.objects.filter(vm_number=reset_vmnum).values('vm_name')[0].values()[0]
+    #DebugInfo.objects.create(text_info=vm_name_value)
     vm_status = CeleryVMsLatestInfo.objects.filter(vm_number=reset_vmnum).values('vm_status')[0].values()[0]
+    #DebugInfo.objects.create(text_info=vm_status)
     #print host_name_value,host_password_value,vm_name_value,vm_status
-    DebugInfo.objects.create(text_info=host_name_value)
-    DebugInfo.objects.create(text_info=host_password_value)
-    DebugInfo.objects.create(text_info=vm_name_value)
-    DebugInfo.objects.create(text_info=vm_status)
-    '''if vm_status == "gray":
+
+
+
+
+    if vm_status == "gray":
         res = set_vm_datastore(host_ip=reset_hostip, host_name=host_name_value, host_password=host_password_value,
                            vm_name=vm_name_value, reservation=reset_data)
         if res:
@@ -150,5 +154,5 @@ def reset_vms_data(request):
         else:
             return render_json({'result': 2})
     else:
-        return render_json({'result': 3})'''
-    return render_json({'result': 1})
+        return render_json({'result': 3})
+    #return render_json({'result': 1})
